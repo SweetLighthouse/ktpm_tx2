a5527ba618c5a4428481ce3dc47927bd6f1d8147
Sweet <sweetlighthousevn@gmail.com>
2024-12-17 14:10:17 +0700

    refactor

--- tests/test_word_database.py
+++ tests/test_word_database.py
@@ -3,10 +3,9 @@ import mongomock
 
 from english_learning_app.database.word_database import WordDatabase
 
-
 class TestUserDatabase(unittest.TestCase):
     def setUp(self):
-        """Set up a mock database."""
+        """Cài đặt mock database."""
         self.mongo_client = mongomock.MongoClient() # mock server
         self.db = WordDatabase() # the dao we want to test for
         self.db.db = self.mongo_client['english_learning_app'] # mock db
@@ -19,21 +18,21 @@ class TestUserDatabase(unittest.TestCase):
             {"username": "john_doe", "english": "handkerchief", "vietnamese": "Khăn tay"},
             {"username": "john_doe", "english": "escapist", "vietnamese": "người chạy trốn"},
             {"username": "john_doe", "english": "ballistic", "vietnamese": "xạ kích"},
+            {"username": "john_doe", "english": "convenient", "vietnamese": "tiện lợi"},
             {"username": "tester01", "english": "unremarkable", "vietnamese": "không đáng kể"},
             {"username": "tester01", "english": "remember", "vietnamese": "nhớ, nhớ lại"},
             {"username": "tester01", "english": "empty", "vietnamese": "trống rỗng"},
             {"username": "tester01", "english": "demand", "vietnamese": "yêu cầu"},
-            {"username": "john_doe", "english": "convenient", "vietnamese": "tiện lợi"},
         ])
         
-    
+   
     def tearDown(self):
-        """Close the MongoClient after each test."""
+        """Đặt lại mỗi lần chạy test case."""
         self.mongo_client.close()
         
 
     def test_add_word(self):
-        """Test adding a new user."""
+        """Test thêm từ mới."""
         result = self.db.add_word('john_doe', 'economy', 'kinh tế')
         
         cursor = self.db.collection.find({
@@ -46,8 +45,8 @@ class TestUserDatabase(unittest.TestCase):
         
 
     def test_add_existing_word(self):
-        """Test adding a word that already exists."""
-        result = self.db.add_word('john_doe', 'convenient', 'tiện lợi') # already existed
+        """Test thêm từ vựng đã tồn tại."""
+        result = self.db.add_word('john_doe', 'convenient', 'tiện lợi') # đã tồn tại
         self.assertFalse(result)
         
         cursor = self.db.collection.find({
@@ -60,14 +59,14 @@ class TestUserDatabase(unittest.TestCase):
         
         
     def test_get_all_words(self):
-        """Test getting all the words from a username"""
+        """Test lấy danh sách từ của 1 user"""
         result = self.db.get_all_words('john_doe')
         self.assertEqual(len(result), 8)
         self.assertTrue(all(word['username'] == 'john_doe' for word in result))
         
 
     def test_delete_word(self):
-        """Test deleting a word."""
+        """Test xoá 1 từ."""
         result = self.db.delete_word('john_doe', 'handkerchief')
         self.assertTrue(result)
         cursor = self.db.collection.find({'username': 'john_doe', 'english': 'a'})
@@ -76,13 +75,13 @@ class TestUserDatabase(unittest.TestCase):
         
 
     def test_update_word(self):
-        """Test updating a word's password."""
+        """Test update 1 từ vựng."""
         result = self.db.update_word('john_doe', 'convenient', 'dễ chịu')
         self.assertTrue(result)
         
 
     def test_word_exists(self):
-        """Test checking if a word exists."""
+        """Test xem từ đã tồn tại hay không."""
         self.assertFalse(self.db.word_exists('john_doe', 'this is a made up word'))
         self.assertTrue(self.db.word_exists('john_doe', 'convenient'))
         
